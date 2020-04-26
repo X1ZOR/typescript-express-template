f08dfcfe79b14aa7a9ba78cbbc1c1c4270628fb9
David Borzek <thejoker77@t-online.de>
2020-04-26 12:22:08 +0200

    refactor: prettier code style

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import * as express from "express";
+import * as express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running in http://localhost:${PORT}`);
+    console.log(`Server is running in http://localhost:${PORT}`);
 });
